34635a5825428f73f1f136e1328c658e747df16b
freemoon99 <soloist132@gmail.com>
2022-10-01 17:51:10 +0900

    6-10번문제

---
+++ 6-10.js
@@ -0,0 +1,37 @@
+// 6번 문제
+// 자바스크립트에서 다음 값은 항상 거짓
+// false
+// 0(영)
+// -0(마이너스 제로)
+// 0n( BigInt제로)
+// '', "", ``(빈 문자열)
+// null
+// undefined
+// NaN
+
+// 7번 문제
+//  JavaScript 식별자는 문자, 밑줄(_) 혹은 달러 기호($)로 시작해야하며 
+// let 은 이미 JavaScript 문법에 존재하는 예약어라 사용이 불가능합니다.
+
+// 8번 문제
+// 84
+
+// 9번 문제
+// const res = year.concat('/',month,'/',day,' ',hour,':', minute,':', second);
+
+// 10번 문제
+// const n = 5;
+// let tree = '';
+
+// for(let i=1; i<=n; i++){
+//   let star = '';
+//   for(let j=1; j<=n-i; j++){
+//     star += ' ';
+//   }
+//   for(let k=1; k<=2*i-1; k++){
+//     star += '*';
+//   }
+//   tree += star + '\n';
+// }
+
+// console.log(tree);
